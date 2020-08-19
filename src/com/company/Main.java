03a9ba3c35609b58fb8b0c1a86077873e2754285
PijusMorkunas <pijuslt28@gmail.com>
2020-08-19 22:15:11 +0300

    Nmbr1

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+// Metodas ivesti ir indeksas 2 atrodo nereikalingi
+        System.out.println("Iveskite kelis zodizus");
+        String sakinys;
+        sakinys = sc.next();
+        ivesti(sakinys);
+        int indeksas1 = 0;
+//        int indeksas2 = sakinys.length(); // pakeisti
+        String z1 = sakinys.substring(indeksas1);
+
+        System.out.println("Pirmas sakinio žodis: " + z1 + ".");
+    }
+
+    private static String ivesti(String sakinys) {
+        return sakinys;
+    }
+
+}
