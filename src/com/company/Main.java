c80df13a6bb7c8cef5bdcfef43f99ba8adcd1892
PijusMorkunas <pijuslt28@gmail.com>
2020-08-21 16:28:18 +0300

    Nmbr1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,11 @@ public class Main {
     public static void main(String[] args) {
 // Metodas ivesti ir indeksas 2 atrodo nereikalingi
         System.out.println("Iveskite kelis zodizus");
-        String sakinys;
-        sakinys = sc.next();
+        String sakinys = sc.next();
         ivesti(sakinys);
         int indeksas1 = 0;
 //        int indeksas2 = sakinys.length(); // pakeisti
         String z1 = sakinys.substring(indeksas1);
-
         System.out.println("Pirmas sakinio žodis: " + z1 + ".");
     }
 
